c8b5983dd92602183209786f7c2467369a9bcba4
Wilfer Salazar <wilfer.salazar@perficient.com>
2025-02-26 08:54:58 -0500

    Initial commit

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,34 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { ProvidePlugin } = require("webpack");
+const commonConfig = require("./webpack.common");
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      //shared: ['react', 'react-dom']
+      shared: packageJson.dependencies
+    }),
+    new ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,35 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { ProvidePlugin } = require("webpack");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8081,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      //shared: ['react', 'react-dom'],
+      shared: packageJson.dependencies,
+    }),
+    new ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
