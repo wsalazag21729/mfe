f72a5b4f466db7c2f18d90f0bf92b7b1af2dac57
wsalazag21729 <wilfersalazar19@gmail.com>
2025-03-04 17:23:49 -0500

    Added auth subapp

---
+++ packages/auth/config/webpack.common.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ProvidePlugin } = require("webpack");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -8,10 +8,10 @@ const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   output: {
-    publicPath: 'http://localhost:8081/'
+    publicPath: 'http://localhost:8082/'
   },
   devServer: {
-    port: 8081,
+    port: 8082,
     historyApiFallback: {
       index: "/index.html",
     },
@@ -21,10 +21,10 @@ const devConfig = {
       template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name: "marketing",
+      name: "auth",
       filename: "remoteEntry.js",
       exposes: {
-        "./MarketingApp": "./src/bootstrap",
+        "./AuthApp": "./src/bootstrap",
       },
       //shared: ['react', 'react-dom'],
       shared: packageJson.dependencies,
