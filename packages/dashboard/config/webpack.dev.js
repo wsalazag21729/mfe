ea2eadce90561e0f7ed47d4b2f0cec1df4cc3414
wsalazag21729 <wilfersalazar19@gmail.com>
2025-03-05 13:45:08 -0500

    Added dashboard

---
+++ packages/dashboard/config/webpack.dev.js
@@ -0,0 +1,41 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { ProvidePlugin } = require("webpack");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8083/",
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap",
+      },
+      //shared: ['react', 'react-dom'],
+      shared: packageJson.dependencies,
+    }),
+    new ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
